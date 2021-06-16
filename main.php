d76e2945d0a26b0c55a5d6b85e589899845e48f3
Erik Wurzer <erik@pixeloution.com>
2021-06-16 06:53:26 -0500

    Update main.php

--- main.php
+++ main.php
@@ -3,7 +3,15 @@
 $DBH = new PDO('mysql:host=localhost;dbname=database_name', $_ENV['username'], $_ENV['password']);
 $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$STH = $DBH->prepare("SELECT $myCol FROM myTbl WHERE id = 55");
+$myCol = $_GET['myCol'];
+$allowedList = ['a','b'];
+
+
+if (in_array($myCol, $allowedList)) {
+  $column = $myCol;
+}
+
+$STH = $DBH->prepare("SELECT $column FROM myTbl WHERE id = 55");
 $STH->execute();
 
   
