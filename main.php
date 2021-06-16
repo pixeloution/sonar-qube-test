89c9fc781bcc3f194cb973c07ffff88407ef5e0a
Erik Wurzer <erik@pixeloution.com>
2021-06-16 07:05:00 -0500

    Update main.php

--- main.php
+++ main.php
@@ -4,7 +4,9 @@ $DBH = new PDO('mysql:host=localhost;dbname=database_name', $_ENV['username'], $
 $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $myCol = $_GET['myCol'];
-$allowedList = ['a','b'];
+
+$STH = $DBH->prepare("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='myDB' AND TABLE_NAME='myTbl'");
+$allowedList = $STH->fetchAll(PDO::FETCH_COLUMN);
 
 # not considered sanitized by itself, must still do sprintf
 if (in_array($myCol, $allowedList)) {
