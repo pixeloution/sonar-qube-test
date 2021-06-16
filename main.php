19b5403e86a359815e57f01bb6896776620fab96
Erik Wurzer <erik@pixeloution.com>
2021-06-16 06:56:27 -0500

    Update main.php

--- main.php
+++ main.php
@@ -6,10 +6,13 @@ $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $myCol = $_GET['myCol'];
 $allowedList = ['a','b'];
 
+# not considered sanitized
+# if (in_array($myCol, $allowedList)) {
+#  $column = $myCol;
+# }
 
-if (in_array($myCol, $allowedList)) {
-  $column = $myCol;
-}
+$column = $allowedList[array_search($myCol, $allowedList)];
+
 
 $STH = $DBH->prepare("SELECT $column FROM myTbl WHERE id = 55");
 $STH->execute();
